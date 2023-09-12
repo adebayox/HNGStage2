c977d95e2d14ae3f6700fd6dd514b673374aa760
mac <dadebayo200@gmail.com>
2023-09-12 20:32:19 +0100

    port updated

--- main.go
+++ main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"github.com/hng/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	port := os.Getenv("PORT") // Use the PORT environment variable
+
+	if port == "" {
+		port = "10000" // Set a default port (e.g., 10000) if PORT is not set
+	}
+
 	r := mux.NewRouter()
 	routes.RegisterPersonRoutes(r)
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe("localhost:3000", r))
+
+	addr := ":" + port
+	log.Printf("Listening on %s...\n", addr)
+	log.Fatal(http.ListenAndServe(addr, r))
 }
